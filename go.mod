ae94fa7ad9410698bdca41602d5acb477fcb0d0b
Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>
2025-04-11 16:51:11 +0200

    feat: enhance Perses MCP server with logging and environment variable support
    - Added log level flag to configure logging verbosity (info, debug).
    - Implemented logging initialization with slog for better log management.
    - Updated Perses client initialization to use Bearer token from environment variable.
    - Changed tool registration from getDashboards to getProjects for clarity.
    - Added warning log for alpha status of Perses MCP.
    
    Signed-off-by: Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>

--- go.mod
+++ go.mod
@@ -8,6 +8,31 @@ require (
 )
 
 require (
+	github.com/brunoga/deep v1.2.4 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nexucis/lamenv v0.5.2 // indirect
+	github.com/perses/common v0.27.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.32.3 // indirect
+	k8s.io/apimachinery v0.32.3 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738 // indirect
+	sigs.k8s.io/controller-runtime v0.20.3 // indirect
+	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.2 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
+)
+
+require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/go-jose/go-jose/v4 v4.0.5 // indirect
@@ -21,7 +46,8 @@ require (
 	github.com/muhlemmer/gu v0.3.1 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
-	github.com/prometheus/client_golang v1.20.5 // indirect
+	github.com/perses/perses-operator v0.1.10
+	github.com/prometheus/client_golang v1.21.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.63.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
@@ -31,7 +57,7 @@ require (
 	github.com/zitadel/oidc/v3 v3.36.1 // indirect
 	github.com/zitadel/schema v1.3.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
-	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/oauth2 v0.28.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
--- main.go
+++ main.go
@@ -3,20 +3,37 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
+
 	persesClient "github.com/perses/perses/pkg/client/api/v1"
 	"github.com/perses/perses/pkg/client/config"
-	"github.com/perses/perses/pkg/model/api"
 	"github.com/perses/perses/pkg/model/api/v1/common"
 )
 
 func main() {
+	var persesServerURL string
+	var logLevel string
 
-	persesClient := initializePersesClient("http://localhost:8080")
+	flag.StringVar(&persesServerURL, "perses-server-url", "http://localhost:8080", "The Perses backend server URL")
+	flag.StringVar(&logLevel, "log-level", "info", "Log level (debug, info, warn, error)")
+	flag.Parse()
+
+	slog.Info("The Perses Server URL is", "url", persesServerURL)
+	slog.Info("Log level set to", "level", logLevel)
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelInfo})))
+	if logLevel == "debug" {
+		slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})))
+	}
+
+	slog.Warn("Perses MCP is in alpha, use at your own risk! ")
+	// Initialize the Perses client
+	persesClient := initializePersesClient(persesServerURL)
 	if persesClient == nil {
 		slog.Error("Failed to initialize Perses client")
 		return
@@ -29,16 +46,16 @@ func main() {
 		server.WithLogging(),
 	)
 
-	slog.Info("Starting Grafana MCP server using stdio transport")
+	slog.Info("Starting Perses MCP server using stdio transport")
 
-	mcpServer.AddTool(getDashboards(persesClient))
+	mcpServer.AddTool(getProjects(persesClient))
 
 	if err := server.ServeStdio(mcpServer); err != nil {
 		slog.Error("Error starting server", "error", err)
 	}
 }
 
-func getDashboards(persesClient persesClient.ClientInterface) (tool mcp.Tool, handler server.ToolHandlerFunc) {
+func getProjects(persesClient persesClient.ClientInterface) (tool mcp.Tool, handler server.ToolHandlerFunc) {
 	return mcp.NewTool("perses_get_projects", mcp.WithDescription("Get all Perses Projects")), func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
 
 		projects, err := persesClient.Project().List("")
@@ -56,11 +73,16 @@ func getDashboards(persesClient persesClient.ClientInterface) (tool mcp.Tool, ha
 
 func initializePersesClient(baseURL string) persesClient.ClientInterface {
 
+	bearerToken := os.Getenv("PERSES_TOKEN")
+	if bearerToken == "" {
+		slog.Error("PERSES_TOKEN environment variable is not set")
+		return nil
+	}
+
 	restClient, err := config.NewRESTClient(config.RestConfigClient{
 		URL: common.MustParseURL(baseURL),
-		NativeAuth: &api.Auth{
-			Login:    "admin",
-			Password: "password",
+		Headers: map[string]string{
+			"Authorization": "Bearer " + bearerToken,
 		},
 	})
 	if err != nil {
