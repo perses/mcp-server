92c01de27da6c59e7f5c0a0476f6af57a134b07f
Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>
2025-11-22 17:12:18 +0100

    adds initial perses mcp server struct
    Signed-off-by: Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>

--- cmd/perses-mcp-server/main.go
+++ cmd/perses-mcp-server/main.go
@@ -5,9 +5,12 @@ import (
 	"os"
 	"strings"
 
+	"github.com/perses/mcp-server/internal/permcp"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+const PERSES_TOKEN = "PERSES_TOKEN"
 
 var version = "version"
 var commit = "commit"
@@ -15,10 +18,28 @@ var date = "date"
 
 var (
 	rootCmd = &cobra.Command{
-		Use:     "mcp-server",
+		Use:     "permcp",
 		Short:   "Perses MCP Server",
 		Long:    "A Perses MCP Server to manage Perses resources",
 		Version: fmt.Sprintf("Version: %s\nCommit: %s\nBuild Date: %s", version, commit, date),
+		RunE: func(_ *cobra.Command, _ []string) error {
+
+			token := viper.GetString(PERSES_TOKEN)
+			if token == "" {
+				return fmt.Errorf("%s not set", PERSES_TOKEN)
+			}
+
+			mcpServerConfig := permcp.MCPServerConfig{
+				Version:         version,
+				PersesServerURL: viper.GetString("perses-server-url"),
+				Token:           token,
+				ReadOnly:        viper.GetBool("read-only"),
+				Transport:       viper.GetString("transport"),
+				Port:            viper.GetString("port"),
+				LogFilePath:     viper.GetString("log-file-path"),
+			}
+			return permcp.RunMCPServer(mcpServerConfig)
+		},
 	}
 )
 
@@ -31,6 +52,7 @@ func init() {
 	// Global flags
 	rootCmd.PersistentFlags().String("perses-server-url", "http://localhost:8080", "The Perses backend server URL")
 	rootCmd.PersistentFlags().String("log-level", "info", "Log level (debug, info, warn, error)")
+	rootCmd.PersistentFlags().String("log-file-path", "", "Path to the log file (if empty, logs go to stderr)")
 	rootCmd.PersistentFlags().String("transport", "stdio", "MCP protocol currently supports 'stdio' and 'http-streamable' transport mechanisms")
 	rootCmd.PersistentFlags().String("port", "8000", "Port to run the HTTP Streamable server on")
 	rootCmd.PersistentFlags().Bool("read-only", false, "Restrict the server to read-only operations")
@@ -41,6 +63,7 @@ func init() {
 	viper.BindPFlag("transport", rootCmd.PersistentFlags().Lookup("transport"))
 	viper.BindPFlag("port", rootCmd.PersistentFlags().Lookup("port"))
 	viper.BindPFlag("read-only", rootCmd.PersistentFlags().Lookup("read-only"))
+	viper.BindPFlag("log-file-path", rootCmd.PersistentFlags().Lookup("log-file-path"))
 
 }
 
