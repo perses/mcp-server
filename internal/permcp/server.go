12e7b9d19322d6f7878506749ba0e9f023b87724
Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>
2025-11-23 08:06:33 +0100

    feat: add permcp command and implement MCP server functionality
    Signed-off-by: Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>

--- internal/permcp/server.go
+++ internal/permcp/server.go
@@ -1,6 +1,16 @@
 package permcp
 
-import "log/slog"
+import (
+	"context"
+	"log/slog"
+	"os/signal"
+	"syscall"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+	v1 "github.com/perses/perses/pkg/client/api/v1"
+	"github.com/perses/perses/pkg/client/config"
+	"github.com/perses/perses/pkg/model/api/v1/common"
+)
 
 type MCPServerConfig struct {
 	// Version of the MCP server
@@ -28,6 +38,64 @@ type MCPServerConfig struct {
 	Port string
 }
 
-func RunMCPServer(cfg MCPServerConfig) error {
+type Input struct {
+	Name string `json:"name" jsonschema:"The name of the person"`
+}
+
+type Output struct {
+	Greeting string `json:"greeting" jsonschema:"The greeting to tell to the user"`
+}
+
+func SayHi(ctx context.Context, req *mcp.CallToolRequest, input Input) (*mcp.CallToolResult, Output, error) {
+	return nil, Output{Greeting: "Hi " + input.Name}, nil
+}
+
+func (cfg *MCPServerConfig) RunMCPServer() error {
+
+	// Create app context
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	// TODO: add log configuration
+
+	// persesClient, err := cfg.initializePersesClient()
+	// if err != nil {
+	// 	return err
+	// }
+
+	// Create a new MCP Server instance
+	mcpServer := mcp.NewServer(&mcp.Implementation{
+		Name:    "perses-mcp-server",
+		Title:   "Perses MCP Server",
+		Version: cfg.Version},
+		&mcp.ServerOptions{
+			HasTools:     true,
+			HasResources: false,
+			HasPrompts:   false,
+		})
+
+	mcp.AddTool(mcpServer, &mcp.Tool{Name: "greet", Description: "say hi"}, SayHi)
+
+	if err := mcpServer.Run(ctx, &mcp.StdioTransport{}); err != nil {
+		slog.Error("Failed to run MCP server", "error", err)
+		return err
+	}
+
 	return nil
+}
+
+func (cfg *MCPServerConfig) initializePersesClient() (v1.ClientInterface, error) {
+	restClient, err := config.NewRESTClient(config.RestConfigClient{
+		URL: common.MustParseURL(cfg.PersesServerURL),
+		Headers: map[string]string{
+			"Authorization": "Bearer " + cfg.Token,
+		},
+	})
+	if err != nil {
+		slog.Error("Failed to create Perses client", "error", err)
+		return nil, err
+	}
+
+	persesClient := v1.NewWithClient(restClient)
+	return persesClient, nil
 }
