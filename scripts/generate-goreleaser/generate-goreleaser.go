c02fbc49e604fc17477adb27697f2fe15b6fdc73
Augustin Husson <husson.augustin@gmail.com>
2026-01-05 11:51:22 +0100

    Merge pull request #62 from perses/nexucis/align-build
    [ignore] align the way to build binaries and docker images
    
    Signed-off-by: Akshay Iyyadurai Balasundaram <akshay.iyyadurai.balasundaram@sap.com>

---
+++ scripts/generate-goreleaser/generate-goreleaser.go
@@ -0,0 +1,32 @@
+// Copyright 2025 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	_ "embed"
+
+	"github.com/perses/perses/scripts/pkg/goreleaser"
+)
+
+//go:embed .goreleaser.base.yaml
+var baseConfig []byte
+
+func main() {
+	goreleaser.Generate(baseConfig, &goreleaser.DockerConfig{
+		ImageName:  "mcp-server",
+		DebugImage: true,
+		BinaryIDs:  []string{"mcp-server"},
+		ExtraFiles: []string{"LICENSE"},
+	})
+}
